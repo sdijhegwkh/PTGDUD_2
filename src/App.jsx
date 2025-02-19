14dade4ad9d3fb2de64f15c48f5da2c10d11b8e9
sdijhegwkh <sprout890@gmail.com>
2025-02-19 11:36:20 +0700

    hoan thanh

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   const [num2, setNum2] = useState(0)
   const [operation, setOperation] = useState('add')
   const [result, setResult] = useState(null)
+  const [chosenButton, setChosenButton] = useState('')
+  const [task, setTask] = useState('')
+  const [tasksList, setTasksList] = useState([])
 
   const calculate = () => {
     let res = 0
@@ -24,7 +27,7 @@ function App() {
         if (num2 !== 0) {
           res = num1 / num2
         } else {
-          res = 'Khong chia cho 0'
+          res = 'Không chia cho 0'
         }
         break
       default:
@@ -32,27 +35,39 @@ function App() {
     }
     setResult(res)
   }
-
+  const handleButtonClick = (language) => {
+    setChosenButton(language)
+  }
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTasksList([...tasksList, task]) 
+      setTask('') 
+    }
+  }
+  const removeTask = (index) => {
+    const updatedTasks = tasksList.filter((_, i) => i !== index)
+    setTasksList(updatedTasks) 
+  }
   return (
     <div className="App">
-      <h1>Xin chao</h1>
+      <h1>Hello</h1>
       <input 
       type="text" 
       value={userInput} 
       onChange={(e) => setUserInput(e.target.value)} 
-      placeholder="Hay nhap gi do "
+      placeholder="Hãy nhập gì đó "
       />
       {userInput && <p>Hello {userInput}!</p>}
-      <h1>May tinh</h1>
+      <h1>Máy tính</h1>
       <input 
         value={num1} 
         onChange={(e) => setNum1(Number(e.target.value))} 
-        placeholder="Nhap so dau tien"
+        placeholder="Nhập số đầu tiên"
       />
       <input 
         value={num2} 
         onChange={(e) => setNum2(Number(e.target.value))} 
-        placeholder="Nhap so thu hai"
+        placeholder="Nhập số thứ hai"
       />
       <div>
         <input 
@@ -95,9 +110,32 @@ function App() {
         />
         <label htmlFor="chia">Chia</label>
       </div>
-      <button onClick={calculate}>Tinh</button>
+      <button onClick={calculate}>Tính</button>
       {result !== null && <p>Ket qua: {result}</p>}
+      <h1>Chon nut </h1>
 
+      <button onClick={() => handleButtonClick('Java')}>Java</button>
+      <button onClick={() => handleButtonClick('C++')}>C++</button>
+      <button onClick={() => handleButtonClick('JavaScript')}>JavaScript</button>
+      <button onClick={() => handleButtonClick('Python')}>Python</button>
+      {chosenButton && <p>Ban chon: {chosenButton}</p>}
+      <h1>Todo List</h1>
+
+      <input
+      type="text"
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+      placeholder="Nhap cong viec"
+    />
+      <button className="button" onClick={addTask}>Them</button>
+      <ul>
+      {tasksList.map((task, index) => (
+      <li key={index}>
+      {task}
+      <button className="remove-button" onClick={() => removeTask(index)}>Xoa</button>
+      </li>
+      ))}
+      </ul>
     </div>
 
   )
